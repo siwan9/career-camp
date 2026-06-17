d4215db82b63454be75703a9c6a78227821be1fc
siwan9 <si.wan208@gmail.com>
2026-06-17 20:04:44 +0900

    fix : 환경변수 오류로 테스트 실패하는거 해결

---
+++ src/main/java/com/wemisson/career_camp/common/config/SpringConfiguration.java
@@ -0,0 +1,7 @@
+package com.wemisson.career_camp.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+}
--- src/test/java/com/wemisson/career_camp/CareerCampApplicationTests.java
+++ src/test/java/com/wemisson/career_camp/CareerCampApplicationTests.java
@@ -2,8 +2,10 @@ package com.wemisson.career_camp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CareerCampApplicationTests {
 
 	@Test
